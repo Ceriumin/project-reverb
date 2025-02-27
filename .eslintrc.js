74a53d3fb4ae2534720ab3f0368b5fa2a6ceab5a
Ceriumin <churns.amblers-0o@icloud.com>
2025-02-27 17:51:29 +0000

    feat: Initial Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react', 'react-native'],
+  env: {
+    'react-native/react-native': true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
