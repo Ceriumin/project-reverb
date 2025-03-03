def488bfc669f0236a2bcc64895d9c1eac37173c
Ceriumin <churns.amblers-0o@icloud.com>
2025-03-03 21:50:56 +0000

    feat(auth): initialize authentication back-end using amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "projectreverbcc934092": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "projectreverbcc934092CustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- index.ts
+++ index.ts
@@ -2,5 +2,8 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './src/navigation/_index';
 import {name as appName} from './app.json';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
 
 AppRegistry.registerComponent(appName, () => App);
+Amplify.configure(config);
