0497f47738b4fc5c2238c3f1bc18da4cc23e8f6e
Ceriumin <churns.amblers-0o@icloud.com>
2025-03-19 22:00:58 +0000

    refactor(auth): add amplify config to index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import {AppRegistry} from 'react-native';
 import App from './src/navigation/_index';
 import {name as appName} from './app.json';
-//import { Amplify } from 'aws-amplify';
-//import awsconfig from './aws-exports';
+
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 AppRegistry.registerComponent(appName, () => App);
